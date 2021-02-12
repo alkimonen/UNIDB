6deb68efd641fab819c7a6c4fa38dce664d018e9
Alkım Önen <39173740+alkimonen@users.noreply.github.com>
2021-02-12 17:22:23 +0300

    Add files via upload

---
+++ OracleSQL/ALKIMDATA_IMPORT.sql
@@ -0,0 +1,30 @@
+@"C:\Users\Alkým Önen\Desktop\UNIDB SCRIPTS\DATA\export_schema.sql";
+
+/*
+select table_name from user_tables;
+drop table building;
+drop table campus;
+drop table classroom;
+drop table course;
+drop table curriculum;
+drop table degrees;
+drop table department;
+drop table entries;
+drop table faculty;
+drop table people;
+drop table section;
+drop table title;
+drop table section_info;
+
+drop sequence building_seq;
+drop sequence class_seq;
+drop sequence course_seq;
+drop sequence curriculum_seq;
+drop sequence department_seq;
+drop sequence entry_seq;
+drop sequence faculty_seq;
+drop sequence pl_seq;
+drop sequence section_info_seq;
+drop sequence seq;
+drop sequence seqtion_seq;
+*/
